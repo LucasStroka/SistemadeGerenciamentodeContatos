d30b3b9624aa5993b3bf3bd9c88f785a3d245143
Lucas Renan Stroka <lucastroka@gmail.com>
2025-08-05 16:22:29 -0300

    editarContato Aprimorado e documentado no readme.md

--- Utils/editarContato.js
+++ Utils/editarContato.js
@@ -5,10 +5,33 @@ const contatos = require("../data/contacts")
 async function editarContato() {
     console.clear()
     listContato()
+    let telefone = " "
+    let nome = " "
+    let email = " "
+
     let idContato = await askQuestion("Selecione o ID do Contato que você deseja editar: ")
-    let nome = await askQuestion("Insira o Nome do Contato: ")
-    let telefone = await askQuestion("Insira o Numero do Contato (1234-1234): ")
-    let email = await askQuestion("Insira o Email do Contato: ")
+
+    let editNome = await askQuestion("Deseja Alterar o Nome? (S/N) ")
+    if (editNome.toUpperCase() == "S") {
+        nome = await askQuestion("Insira o Nome do Contato: ")
+    } else {
+        nome = contatos[idContato-1].nome
+    }
+
+    let editTelefone = await askQuestion("Deseja Alterar o Telefone? (S/N) ")
+    if (editTelefone.toUpperCase() == "S") {
+        telefone = await askQuestion("Insira o Numero do Contato (1234-1234): ")
+    } else {
+        telefone = contatos[idContato-1].telefone
+    }
+
+    let editEmail = await askQuestion("Deseja Alterar o Email? (S/N) ")
+    if (editEmail.toUpperCase() == "S") {
+        email = await askQuestion("Insira o Email do Contato: ")
+    } else {
+        email = contatos[idContato-1].email
+    }
+
 
     contatos[idContato-1] = {nome, telefone, email}
     console.log("Editado com Sucesso!");
